b45e644cf9ce143475982b874ed90eb19870c12b
Владимир Агапов <rigroll@yandex.ru>
2023-05-01 21:53:14 +0300

    Исправлено

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,16 +2,13 @@ let editor = document.getElementById("editor");
 let button = document.getElementById("button");
 
 window.addEventListener("load", function() {
-    if (localStorage.text) {
-        editor.value = localStorage.getItem("text");
-    } else {
-        editor.addEventListener("input", function() {
-            localStorage.setItem("text", editor.value);
-        });
-    };
+    editor.value = localStorage.getItem("text");
+    editor.addEventListener("input", function() {
+        localStorage.setItem("text", editor.value);
+    });
 });
 
 button.addEventListener("click", function() {
-    localStorage.clear()
+    localStorage.removeItem("text")
     editor.value = "";
 });
